ce478b62bd13e1b1828a62c81a441b75bb552c51
Happykhamissa <elmansourikhamissa@gmail.com>
2023-10-04 17:06:32 +0100

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
-def check_case(c):
-    if ord('a') <= ord(c) <= ord('z'):
-        print(f"{c} is lower")
-    elif ord('A') <= ord(c) <= ord('Z'):
-        print(f"{c} is upper")
+    if ord("a") <= ord(c) <= ord("z")
+        return True
     else:
-        print(f"{c} is not a letter")
-
-check_case('a')
-check_case('H')
-check_case('A')
-check_case('3')
-check_case('g')
+        return False
