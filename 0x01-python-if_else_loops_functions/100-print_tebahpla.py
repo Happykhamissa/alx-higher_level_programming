850932617760db38094c55520390cdb0bf93be1d
Happykhamissa <elmansourikhamissa@gmail.com>
2023-10-04 22:15:23 +0100

    Update 100-print_tebahpla.py and 12-main.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,1 @@
-#!/usr/bin/python3
-for ascii_value in range(ord('z'), ord('a') - 1, -1):
-    char = chr(ascii_value)
-    if (ord('z') - ascii_value) % 2 == 1:
-        char = char.upper()
-    print("{}".format(char), end='')
+
