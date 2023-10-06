e89231a4018bb45066fb72ba6d2968050f199aaa
Happykhamissa <elmansourikhamissa@gmail.com>
2023-10-06 19:55:30 +0100

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-import sys
+import hidden_4
+
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
 if __name__ == "__main__":
-    total = 0
-    for arg in sys.argv[1:]:
-        total += int(arg)
-    print(total)
+    discovr()
