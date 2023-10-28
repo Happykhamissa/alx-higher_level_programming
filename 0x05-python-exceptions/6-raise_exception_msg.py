6a21812d56a580263c7fb4f00c3ba594da2d744a
Happykhamissa <elmansourikhamissa@gmail.com>
2023-10-28 09:37:12 +0100

    Raise a message

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """
+    Raises a name exception with a custom message.
+
+    Args:
+        message (str, optional): The custom message for the exception.
+    """
+    raise NameError(message)
