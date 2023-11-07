be41c690459451d4319cb87eeb91f0abc79bbcf1
Happykhamissa <elmansourikhamissa@gmail.com>
2023-11-07 20:23:42 +0100

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a class"""
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Parameters:
+    obj (object): The object for which attributes and methods are to be listed.
+
+    Returns:
+    list: A list of available attributes and methods of the provided object.
+    """
+    return [attr for attr in dir(obj)]
